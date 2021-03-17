e99b7f633f14d8df16961ab41cdee3d5a3121736
Andrea Gori <Andrea.fr.gori@gmail.com>
2021-03-17 17:58:11 +0100

    completo senza css

--- main.js
+++ main.js
@@ -5,25 +5,24 @@ $(document).ready(function () {
 
     /* aggiungo gli elementi alla to do list */
     listItem.children('span.title').text(listVal);
-    
+
     /* funzione per eliminare l'elemento fatto */
     listItem.children('span.cancella').click(function () {
       $(this).parent().remove()
     })
 
+    listItem.children('span.edit').click(function () {
+      $(this).prev().show().val();
+      $(this).siblings('span.title').hide()
+    })
 
-
-
-
-
-
-
-
-
-
-
-
-
+    listItem.children('.modifica').keyup(function (e) {
+      if (e.which == 13 ) {
+        var title = $(this).val();
+        $(this).hide();
+        $(this).siblings('span.title').text(title).show();
+      }
+    })
 
     $('#todo-list').append(listItem);
   })
